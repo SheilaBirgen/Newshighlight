812d2c29274bb862640b6e80862ec14749b7244f
SheilaBirgen <jeronobergen@gmail.com>
2020-02-03 12:10:01 +0300

    create class articles

--- python/NewsHighlight/app/models.py
+++ python/NewsHighlight/app/models.py
@@ -10,3 +10,18 @@ class Source:
         self.description = description
         self.url= url
         self.country=country
+
+class Articles:
+    """
+    Defines what our articles object will look like
+    """
+
+    def __init__(self,id,name,author,title,description,url,urlToImage,publishedAt):
+        self.id = id
+        self.name = name
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
