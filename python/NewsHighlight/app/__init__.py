9ccd028b2aff1c78aa2bf824e1ad504d612dc8e4
SheilaBirgen <jeronobergen@gmail.com>
2020-02-03 17:08:53 +0300

    rectify issues in manage.py

--- python/NewsHighlight/app/__init__.py
+++ python/NewsHighlight/app/__init__.py
@@ -4,21 +4,6 @@ from config import config_options
 
 bootstrap = Bootstrap()
 
-# def create_app(config_name):
-
-#     app = Flask(__name__)
-
-#     #creating app configs
-#     app.config.from_object(config_options[config_name])
-
-#     # Initializing flask extensions
-#     bootstrap.init_app(app)
-
-#     # Registering the blueprint
-#     from .main import main as main_blueprint
-#     app.register_blueprint(main_blueprint)
-
-#     return app
 def create_app(config_name):
 
     app = Flask(__name__)
